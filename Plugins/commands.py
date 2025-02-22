344d7cb360cd454f98034d9383324a0ae659a74f
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-22 22:27:31 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -1,5 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 import logging
@@ -9,7 +7,6 @@ from pyrogram import filters
 from bot import channelforward
 from config import Config
 from translation import Translation
-
 
 ################################################################################################################################################################################################################################################
 # start command
@@ -34,3 +31,28 @@ async def about(client, message):
     )
 
 ################################################################################################################################################################################################################################################
+# addchannel command
+
+@channelforward.on_message(filters.command("addchannel") & filters.private & filters.incoming)
+async def add_channel(client, message):
+    if len(message.command) < 3:
+        await message.reply("Usage: /addchannel <pickup_channel_id> <target_channel_id>")
+        return
+    pickup, target = message.command[1], message.command[2]
+    channel_mappings[pickup] = target
+    await message.reply(f"Added mapping: {pickup} → {target}")
+
+################################################################################################################################################################################################################################################
+# removechannel command
+
+@channelforward.on_message(filters.command("removechannel") & filters.private & filters.incoming)
+async def remove_channel(client, message):
+    if len(message.command) < 2:
+        await message.reply("Usage: /removechannel <pickup_channel_id>")
+        return
+    pickup = message.command[1]
+    if pickup in channel_mappings:
+        del channel_mappings[pickup]
+        await message.reply(f"Removed mapping for {pickup}")
+    else:
+        await message.reply("Channel not found in mappings.")
